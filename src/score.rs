5cf8f032fc235886d9f3693698e877c5f7c0bc97
Ryan Whittingham <ryanwhittingham89@gmail.com>
2022-03-03 18:05:04 -0800

    clippy -- use char

--- src/score.rs
+++ src/score.rs
@@ -4,6 +4,6 @@ use crate::db::Dir;
 
 /// Score a query against a directory
 pub fn score(args: &[String], dir: &Dir) -> u32 {
-    let fuzz_score = token_sort_ratio(&args.join(" "), &dir.dir.replace("/", " "), true, true);
+    let fuzz_score = token_sort_ratio(&args.join(" "), &dir.dir.replace('/', " "), true, true);
     fuzz_score.into()
 }
